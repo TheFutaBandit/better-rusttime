5229ad56a99ece9a6dbbed99f8f6ec4cb96b246b
TheFutaBandit <prameshanand2021@gmail.com>
2025-07-11 12:26:25 +0530

    base routing successful

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{get, handler, listener::TcpListener, post, web::{Json, Path}, IntoResponse, Route, Server};
+
+use store::Store;
+
+use crate::{request_inputs::CreateWebsiteInput, request_outputs::CreateWebsiteOutput};
+
+pub mod request_inputs;
+pub mod request_outputs;
+
+#[handler]
+fn get_website(Path(website_id): Path<String>) -> String {
+    format!("hello")
+}
+
+#[handler]
+fn create_website(Json(data) : Json<CreateWebsiteInput>) -> Json<CreateWebsiteOutput> {
+    // make db call
+    let url = data.url;
+
+    let store = Store {};
+    let id = store.create_website();
+
+    let response = CreateWebsiteOutput{
+        id
+    };
+
+    Json(response)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new()
+        .at("/website/:website_id", get(get_website))
+        .at("/website", post(create_website));
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+        .run(app)
+        .await
 }
